dfb59cfae120782ea55291b383d299bc8b4b9461
selimmh <selim.mht@gmail.com>
2023-04-22 19:34:57 +0300

    fdd structure

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mantine/core";
+
+function App() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ThemeProvider } from "./ThemeProvider.tsx";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
