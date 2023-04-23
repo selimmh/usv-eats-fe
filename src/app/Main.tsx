302c426d7dfc360294613669ad7584971b733165
selimmh <selim.mht@gmail.com>
2023-04-23 21:57:44 +0300

    boilerplate pages

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { ThemeProvider } from "./ThemeProvider.tsx";
+import { App } from "./App.js";
+
+import { BrowserRouter } from "react-router-dom";
+import { Provider as JotaiProvider } from "jotai";
+
+import { ThemeProvider } from "./utils/ThemeProvider.js";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <JotaiProvider>
+          <App />
+        </JotaiProvider>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
